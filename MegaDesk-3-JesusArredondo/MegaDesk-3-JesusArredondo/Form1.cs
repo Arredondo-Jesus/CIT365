d88ff173c2419dad60bdfd557fc26db46e82ec81
Jesus Arredondo <chuyarredondo@gmail.com>
2017-09-24 00:12:57 -0500

    Re-arrenging folders

--- MegaDesk-3-JesusArredondo/MegaDesk-3-JesusArredondo/Form1.cs
+++ MegaDesk-3-JesusArredondo/MegaDesk-3-JesusArredondo/Form1.cs
@@ -16,5 +16,10 @@ namespace MegaDesk_3_JesusArredondo
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
