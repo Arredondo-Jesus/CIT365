3fce64906edb35218de22dfa72e4d350a4ec878a
Jesus Arredondo <chuyarredondo@gmail.com>
2017-09-26 21:25:46 -0500

    Updating again

--- MegaDesk-3-JesusArredondo/MegaDesk-3-JesusArredondo/Desk.cs
+++ MegaDesk-3-JesusArredondo/MegaDesk-3-JesusArredondo/Desk.cs
@@ -8,5 +8,34 @@ namespace MegaDesk_3_JesusArredondo
 {
     class Desk
     {
+        private float width;
+        private float deph;
+        private int drawers;
+        private enum Materials { }
+
+        public float getWidth() {
+            return this.width;
+        }
+
+        public void setWidth(float width) {
+            this.width = width;
+        }
+
+        public float getDeph() {
+            return this.deph;
+        }
+
+        public void setDeph(float deph) {
+            this.deph = deph;
+        }
+
+        public int getDrawers() {
+            return this.drawers;
+        }
+
+        public void setDrawers(int drawers) {
+            this.drawers = drawers;
+        }
+
     }
 }
