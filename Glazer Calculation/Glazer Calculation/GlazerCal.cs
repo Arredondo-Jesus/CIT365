17ab2c1498544900a891d6c23647e7c3cac13d37
Jesus Arredondo <chuyarredondo@gmail.com>
2017-10-12 02:08:05 -0500

    Adding UWP examples

---
+++ Glazer Calculation/Glazer Calculation/GlazerCal.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Glazer_Calculation
+{
+    class GlazerCal
+    {
+        public int width { set; get; }
+        public int height { set; get; }
+        public double woodLegth { set; get; }
+        public double glassArea { set; get; }
+
+        public double calcWoodLength(MainPage mainPage) {
+            int width = this.width = mainPage.getWidth();
+            int height = this.height = mainPage.getHeight();
+
+            double woodLength = 2 * (width * height) * 3.25;
+      
+            return woodLength;
+        }
+
+        public double calcGlassArea(MainPage mainPage)
+        {
+            int width = this.width = mainPage.getWidth();
+            int height = this.height = mainPage.getHeight();
+
+            double glassAarea = 2 * (width * height) * 3.25;
+
+            return glassAarea;
+        }
+    }
+    
+}
